98d67e6c7dba5c17ea9cea84e86c55101c6cbbf5
Rijan Tiwari <reezantiwaree@gmail.com>
2023-09-05 13:09:17 +0545

    Variable Hoisting

---
+++ Javascript/variableHoisting.js
@@ -0,0 +1,14 @@
+var a;
+console.log("The value of  a is " + a);
+console.log("The value of  b is " + b);
+var b;
+var p = 7;
+console.log("Value of p is " + p);
+console.log("Value of q is " + q);
+var q = 10;
+
+let x;
+console.log("The value of  x is " + x);
+
+console.log("The value of  y is " + y); //ReferenceError
+let y;
