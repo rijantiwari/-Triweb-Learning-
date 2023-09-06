44c6c6ee66f1007b3b58519f0550b04043b2a457
Rijan Tiwari <reezantiwaree@gmail.com>
2023-09-06 19:38:02 +0545

    Loop

---
+++ Javascript/forInLoop.js
@@ -0,0 +1,9 @@
+let myObj = {
+  fname: "Rijan",
+  lName: "Tiwar",
+  phone: 985158,
+};
+
+for (let key in myObj) {
+  console.log(`${key} = ${myObj[key]}`);
+}
---
+++ Javascript/forOfLoop.js
@@ -0,0 +1,4 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+for (let i of arr) {
+  console.log(i);
+}
