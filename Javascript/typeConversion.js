ade5b05876e59c4de32e6de1632793c6112decdd
Rijan Tiwari <reezantiwaree@gmail.com>
2023-09-06 16:19:52 +0545

    Type Conversion

---
+++ Javascript/typeConversion.js
@@ -0,0 +1,23 @@
+let n = 1;
+console.log(typeof n);
+
+n = "Hi Rijan ";
+console.log(typeof n);
+let num1 = "3";
+let num2 = 5;
+console.log(num1 + num2);
+let n3 = "3";
+let n4 = 5;
+console.log(parseInt(n3) + n4);
+let n5 = "3.5";
+let n6 = 5;
+console.log(parseFloat(n5) + n6);
+let n7 = "8";
+let n8 = 5;
+console.log(+n7 + n8);
+console.log(n7 - n8);
+console.log(n7 * n8);
+console.log(n7 / n8);
+n9 = "101";
+console.log(parseInt(n9, 2));
+console.log(parseInt(n9, 16));
