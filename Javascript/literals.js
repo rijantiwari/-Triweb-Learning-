d5d92fdfdaf56ffd7d74ff29125127e3a0a2222d
Rijan Tiwari <reezantiwaree@gmail.com>
2023-09-06 16:32:45 +0545

    literals

---
+++ Javascript/literals.js
@@ -0,0 +1,11 @@
+console.log("Decimal", 117);
+console.log("Octal", 0o77);
+console.log("hexa decimal", 0x00111);
+console.log("binary", 0b0011);
+console.log("Binary", 0b0011);
+
+console.log("Object Literal");
+let obj = { " ": "rijan", "!": 955155 };
+console.log(obj[" "], obj["!"]);
+let obj1 = { name: "rijan", phone: 955155 };
+console.log(obj1.name, obj1.phone);
