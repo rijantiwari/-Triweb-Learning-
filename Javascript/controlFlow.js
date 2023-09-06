d36dda1dc9b86d5b5a778ebd5c98a22dc55886c9
Rijan Tiwari <reezantiwaree@gmail.com>
2023-09-06 16:49:00 +0545

    control flow

---
+++ Javascript/controlFlow.js
@@ -0,0 +1,21 @@
+// block statement
+if (true) {
+  let x = 5;
+  console.log(x);
+}
+
+// conditional Statement
+let n = 3;
+let m = 4;
+if (n > m) {
+  console.log("n is greater");
+} else {
+  console.log("m is greater");
+}
+
+// Falsy Values
+if (false) {
+  console.log("In if  1");
+} else {
+  console.log("In else 1 ");
+}
