3ddc0797b079f78d6f21d980edeb5db207d38980
Rijan Tiwari <reezantiwaree@gmail.com>
2023-09-09 18:54:03 +0545

    Form completed

--- Projects/HTML-CSS-Project/form.js
+++ Projects/HTML-CSS-Project/form.js
@@ -1,40 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Registration Form</title>
-      <link rel="stylesheet" type="text/css" href="form.css">
-</head>
-<body>
-    <h3> User Registration Form</h3>
-    <form>
-      <label for="fname">First Name:</label><br>
-      <input type="text" id="fname" name="fname"><br>
-      <label for="lname">Last Name:</label><br>
-      <input type="text" id="lname" name="lname"><br>
-      <label for="email">Email:</label><br>
-      <input type="email" id="email" name="email"><br>
-      <label for="contact">Contact:</label><br>
-      <input type="tel" id="contact" name="contact"><br>
-      <label for="dob">Date of Birth:</label><br>
-      <input type="date" id="date" name="date"><br>
-      <label for="age">Age:</label><br>
-      <input type="number" id="age" name="age"><br>
+document.addEventListener("DOMContentLoaded", function () {
+  const dobInput = document.getElementById("dob");
+  const ageInput = document.getElementById("age");
+  const submitBtn = document.getElementById("submit-btn");
+  const form = document.getElementById("registration-form");
 
-      <label for="Gender">Gender</label><br>
-      <input type="radio" id="male" name="gender">
-      <label for="male">Male</label>
-      <input type="radio" id="female" name="gender">
-      <label for="female">Female</label>
-      <input type="radio" id="other" name="gender">
-      <label for="other">Other</label><br>
+  dobInput.addEventListener("change", function () {
+    const dob = new Date(this.value);
+    const today = new Date();
+    const age = Math.floor((today - dob) / (365.25 * 24 * 60 * 60 * 1000));
+    ageInput.value = age;
+  });
 
-      <label for="resume">Resume:</label>
-      <input type="file" id="resume" name="resume" accept=".pdf"><br>
+  form.addEventListener("submit", function (e) {
+    const emailInput = document.getElementById("email");
+    const contactInput = document.getElementById("contact");
+    const emailError = document.getElementById("email-error");
+    const contactError = document.getElementById("contact-error");
 
-    <button type ="submit" id="butn"class="btn">Submit</button>
-    </form>
-    <script src="form.js"></script>
-</body>
-</html>
+    if (!emailInput.validity.valid) {
+      e.preventDefault();
+      emailError.textContent = "Please enter a valid email address.";
+    } else {
+      emailError.textContent = "";
+    }
+
+    if (
+      contactInput.value.length !== 10 ||
+      !/^\d{10}$/.test(contactInput.value)
+    ) {
+      e.preventDefault();
+      contactError.textContent = "Contact number should be exactly 10 digits.";
+    } else {
+      contactError.textContent = "";
+    }
+  });
+});
