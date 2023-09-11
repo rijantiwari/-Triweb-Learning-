9b1e3fa77ed248450f838cf73e36bb7f681e314d
Rijan Tiwari <reezantiwaree@gmail.com>
2023-09-11 15:23:41 +0545

    Constant

---
+++ Es6/Constant.js
@@ -0,0 +1,14 @@
+// var a = 5;
+// console.log(a);
+// var a = 7;
+
+// const b = 9;
+// console.log(b);
+// b = 6;
+
+const obj = { name: "Rijan", age: 28 };
+console.log("Object before", obj);
+// obj = { name: "Rijan", age: 30 };
+obj.name = "Rijan";
+obj.age = 40;
+console.log("after", obj);
