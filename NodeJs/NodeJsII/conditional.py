2c1df712ed5b1bf1dff87a1882d6f4d7c32b1acb
Rijan Tiwari <reezantiwaree@gmail.com>
2023-09-21 14:34:49 +0545

    Conditional statement in python

---
+++ NodeJs/NodeJsII/conditional.py
@@ -0,0 +1,11 @@
+print("Enter the Height of the Person : ");
+height1 = int(input());
+print("Enter height of the second Person: ");
+height2 = int(input());
+
+
+if(height1 > height2):
+    print("Person 1 is taller");
+
+else :
+    print("Person 2 is taller ")
