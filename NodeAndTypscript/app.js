bfc4e942471be0b3a43db7435d9de3d989fd7928
Rijan Tiwari <reezantiwaree@gmail.com>
2023-09-29 13:30:02 +0545

    Restore deleted folder

--- NodeAndTypscript/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const userRoute = require("./routes/user");
-app.get("/", (req, res) => {
-  res.send("Hi From Server ");
-});
-app.use("/user", userRoute);
-
-app.listen(process.env.PORT);
--- NodeAndTypscript/models/user.js
+++
@@ -1,6 +0,0 @@
-const addUserToDB = (username, password) => {
-  console.log(username, password);
-  return "User Registration Done";
-};
-
-module.exports = { addUserToDB };
