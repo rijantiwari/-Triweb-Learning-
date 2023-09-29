eba556f8e0bda2d0161f71cec7952ff127f16c12
Rijan Tiwari <reezantiwaree@gmail.com>
2023-09-29 12:20:53 +0545

    Added nodemon to the project

--- NodeAndTypscript/app.js
+++ NodeAndTypscript/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.get("/hello", (req, res) => {
+  res.send(" Hello From Server ");
+});
 app.get("/", (req, res) => {
-  res.send("HellO I'm Running ");
+  res.send("Hi ");
 });
 
 app.listen(3000);
